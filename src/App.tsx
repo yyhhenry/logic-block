cbe80b5ce9b0ff59c9be2483a30a0723f9e33c1f
yyhhenry <yyhhenry@foxmail.com>
2022-06-12 23:06:11 +0800

    初步定下前端风格

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Layer, Rect, Stage, Text, Group, Circle } from 'react-konva';
-import GroupInImage from './component/GroupInImage';
+import { AppHeader } from './component/AppHeader'
 
-
-interface MyInputProps {
-  type: string;
-}
-
-interface MyInputStates {
-  value: string;
-}
-
-
-
-class MyInput extends React.Component<MyInputProps, MyInputStates> {
-  state = { value: '' };
-  render(): React.ReactNode {
-    requestAnimationFrame(() => this.forceUpdate());
-    const width = window.innerWidth / 5;
-    const height = window.innerHeight / 5;
-    const dateValue = new Date().toString();
-    return (
-      <div>
-        <input
-          type={this.props.type}
-          onInput={ev => {
-            this.setState({
-              value: ev.currentTarget.value
-            });
-          }}
-          style={{
-            margin: '10px',
-            background: 'rgb(255,255,255,.8)',
-          }}
-        ></input>
-        <Stage id={'stage'} width={width} height={height}>
-          <Layer>
-            <Rect x={0} y={0} width={width} height={height} fill={'white'}></Rect>
-            <Group globalCompositeOperation='xor'>
-              <GroupInImage group={
-                <Group>
-                  <Circle x={width / 3} y={height / 2} radius={width / 5} fill={'white'}></Circle>
-                  <Circle x={width * 2 / 3} y={height / 2} radius={width / 5} fill={'white'}></Circle>
-                </Group>
-              } width={width} height={height} />
-            </Group>
-            <Text text={dateValue} x={20} y={60} width={width - 40} fill={'white'} fontSize={20} fontFamily={'微软雅黑'} globalCompositeOperation='xor' />
-          </Layer>
-          <Layer>
-            <Text text={this.state.value} x={10} y={10} fill={'gray'} fontSize={20} fontFamily={this.state.value === '233' ? '宋体' : '微软雅黑'} />
-          </Layer>
-        </Stage>
-      </div>
-    );
-  }
-}
-interface AppHeaderProps {
-}
-interface AppHeaderStates {
-}
-class AppHeader extends React.Component<AppHeaderProps, AppHeaderStates> {
-  render(): React.ReactNode {
-    return (
-      <header
-        style={{
-          position: 'fixed',
-          top: 0, left: 0, right: 0, height: 60,
-          background: 'rgba(255,255,255)',
-        }}
-      >
-        { }
-      </header>
-    );
-  }
-}
 
 class App extends React.Component {
   render(): React.ReactNode {
     return (
-      <div className="App">
-        <AppHeader />
-        <header className="App-header">
-          <MyInput type={'input'}></MyInput>
-        </header>
-      </div>
+      <AppHeader pageContent={
+        undefined
+      } />
     );
   }
 }
---
+++ src/component/HoveredNode.tsx
@@ -0,0 +1,32 @@
+import React, { EventHandler, SyntheticEvent } from 'react';
+
+function mergeListener<T extends SyntheticEvent<any>>(newFunc: EventHandler<T>, originFunc: EventHandler<T> | undefined): EventHandler<T> {
+    return ev => {
+        originFunc && originFunc(ev);
+        newFunc(ev);
+    };
+}
+export interface HoveredNodeProps {
+    style?: (hoverMark: boolean) => React.CSSProperties;
+    content: (hoverMark: boolean) => React.ReactNode;
+    originProps?: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+}
+export interface HoveredNodeState {
+    hoverMark: boolean;
+}
+export class HoveredNode extends React.Component<HoveredNodeProps, HoveredNodeState>{
+    state: Readonly<HoveredNodeState> = { hoverMark: false };
+    render(): React.ReactNode {
+        let style = this.props.style && this.props.style(this.state.hoverMark);
+        let props = { ...this.props.originProps, style };
+        props.onMouseEnter = mergeListener(() =>
+            this.setState({ hoverMark: true }), props.onMouseEnter);
+        props.onMouseLeave = mergeListener(() =>
+            this.setState({ hoverMark: false }), props.onMouseLeave);
+        return (
+            <div {...props}>
+                {this.props.content && this.props.content(this.state.hoverMark)}
+            </div>
+        );
+    }
+}
